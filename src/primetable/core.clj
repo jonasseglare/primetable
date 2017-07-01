729f70ab2d419a69d8539d425c64b0fcc59d6803
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-07-01 10:48:39 +0200

    add a core

---
+++ src/primetable/core.clj
@@ -0,0 +1,29 @@
+(ns primetable.core)
+
+(defn composite? [table i]
+  (not (nil? (nth table i))))
+
+(defn mark [table i f]
+  (if (nil? (nth table i))
+    (assoc table i f)
+    table))
+
+(defn mark-primes-sub [table f]
+  (let [n (count table)]
+    (reduce (fn [table i] (mark table i f))
+            table
+            (take-while
+             #(< % n)
+             (map #(* f %)
+                  (iterate inc 2))))))
+
+(defn mark-primes [table i]
+  (if (or (< i 2) (composite? table i))
+    table
+    (mark-primes-sub table i)))
+
+(defn make-first-factor-table [n]
+  (reduce
+   mark-primes
+   (vec (repeat n nil))
+   (range n)))
