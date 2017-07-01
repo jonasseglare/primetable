39d1a7f4b1d9cea7f9440ea4a82d74aabd17e173
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-07-01 10:58:06 +0200

    bra

--- src/primetable/core.clj
+++ src/primetable/core.clj
@@ -22,7 +22,10 @@
     table
     (mark-primes-sub table i)))
 
-(defn make-first-factor-table [n]
+(defn make-first-factor-table
+  "Create a vector where element i is nil if it is a prime number, 
+  or the first prime factor if it is a composite number"
+  [n]
   (reduce
    mark-primes
    (vec (repeat n nil))
--- test/primetable/core_test.clj
+++ test/primetable/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [primetable.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest table-test
+  (is (= (make-first-factor-table 10)
+         [nil nil nil nil 2 nil 2 nil 2 3])))
